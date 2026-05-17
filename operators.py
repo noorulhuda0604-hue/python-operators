d5b028071ab16bd229eeba9d5f0686c2ad6395fa
noorulhuda0604-hue <noorulhuda0604@gmail.com>
2026-05-17 16:17:52 +0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,34 @@
+#Arthmetic operators 
+a=2
+b=4
+print("addition:",a+b)
+# add with 3 variable 
+a=2
+b=4
+c=a+b
+print("addition:",c)
+# substract 
+a=20
+b=10
+print("substract:",a-b)
+# sub with 3rd variable
+a=60
+b=10
+c=a-b
+print("substract:",c)
+# divide
+a=40
+b=2
+print("Divide:",a/b)
+# modulus
+a=50
+b=3
+print("modulus:",a%b)
+# multiplication
+a=2
+b=4
+print("multiple:",a*b)
+# Power
+a=4
+b=7
+print("power:",a**b)
